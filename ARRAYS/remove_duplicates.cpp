54481f72b978f37b2eaa58d42204568bf85b6427
Manasa Indukuri <isrmanasa@gmail.com>
2026-08-30 20:32:03 +0530

    remove duplicates from sorted array

--- ARRAYS/remove_duplicates.cpp
+++ ARRAYS/remove_duplicates.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int k=0;
-    int arr[6]={1,1,2,2,3,4};
-    for(int i=1;i<6;i++){
-        if(arr[i]!=arr[k-1]){
-            arr[k]=arr[i];
-            k++;
+    int i=0;
+    int j=1;
+    int num1[6]={1,1,2,2,3,4};
+    while(j<6){
+        if(num1[i]==num1[j]){
+            j++;
+        }
+        if(num1[i]!=num1[j]){
+            i++;
+            num1[i]=num1[j];
+            j++;
         }
     }
-    for(int i=0;i<k;i++){
-        cout<<arr[i]<<" ";
+    for(int k=0;k<=i;k++){
+        cout<<num1[k]<<" ";
     }
 }
